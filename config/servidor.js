6920f4f77298f1f1641fb18ac214deb8ad701eb7
Mario2930 <100131163+Mario2930@users.noreply.github.com>
2022-06-15 08:32:46 -0300

    Add files via upload

--- config/servidor.js
+++ config/servidor.js
@@ -3,7 +3,7 @@ const express = require('express')
 //executar o express
 const app = express()
 //definir a porta do servidor local
-const porta = 3535
+const porta = process.env.PORT || 3535
 
 //defenir a pasta dos arquivos estaticos (css, imagens, jquery)
 app.use(express.static('./assets'))
